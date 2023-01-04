d6ea10abb357ce63ea44e4c3424d138c7825d1dc
seunghye910326 <121917723+seunghye910326@users.noreply.github.com>
2023-01-04 17:39:59 +0900

    Revert "ddd"
    This reverts commit b7e457d190d910f04947adf7ba53edd7a5cea47a.

--- Nongbusim/src/main/java/com/kh/nbs/account/controller/AccountController.java
+++ Nongbusim/src/main/java/com/kh/nbs/account/controller/AccountController.java
@@ -1,6 +1,9 @@
 package com.kh.nbs.account.controller;
 
 public class AccountController {
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7f36f0425b3296b2a8e98d171734e63f7770dc4f
 
 }
--- Nongbusim/src/main/java/com/kh/nbs/farm/model/vo/Farm.java
+++ Nongbusim/src/main/java/com/kh/nbs/farm/model/vo/Farm.java
@@ -2,12 +2,17 @@ package com.kh.nbs.farm.model.vo;
 
 public class Farm {
 
-
+<<<<<<< HEAD
+}
+=======
 	private int farmNo;
 	private int farmer;
 	private String farmName;
 	private String localCode;
-
+<<<<<<< HEAD
 	private String address;
-
 }
+>>>>>>> fa50775b631f3c3005e84f7da64fc7f7fe49a8a2
+=======
+}
+>>>>>>> 7f36f0425b3296b2a8e98d171734e63f7770dc4f
