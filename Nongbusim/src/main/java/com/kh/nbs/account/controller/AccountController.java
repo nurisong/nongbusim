27972f6f6ea59d1412c9d75321f1c318cf072ebc
SangminLee <121650362+fara2828@users.noreply.github.com>
2023-01-05 12:24:55 +0900

    g
    g

--- Nongbusim/src/main/java/com/kh/nbs/account/controller/AccountController.java
+++ Nongbusim/src/main/java/com/kh/nbs/account/controller/AccountController.java
@@ -1,7 +1,25 @@
 package com.kh.nbs.account.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.nbs.account.model.service.AccountService;
+
+@Controller
 public class AccountController {
 
-	private String name;
-
+	@Autowired
+	private AccountService accountService;
+	
+	@RequestMapping("list.ac")
+	public String accountlist() {
+		return "member/myPageFarmer/account/accountListView" ;
+		
+	}
+	
+	
+	
+		
+	
 }
