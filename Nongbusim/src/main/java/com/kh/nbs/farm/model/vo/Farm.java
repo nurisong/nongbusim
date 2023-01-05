fa006dfc5e06fd03d841be73e3f4b8287508fccb
nurisong <noooori0827@gmail.com>
2023-01-05 18:10:41 +0900

    farm 추가

---
+++ Nongbusim/src/main/java/com/kh/nbs/farm/model/vo/Farm.java
@@ -0,0 +1,13 @@
+package com.kh.nbs.farm.model.vo;
+
+public class Farm {
+
+	private int farmNo;
+	private int farmer;
+	private String farmName;
+	private String localCode;
+	private String address;
+	private int age;
+	
+}
+
