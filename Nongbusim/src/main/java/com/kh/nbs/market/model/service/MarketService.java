e264e024f1e8d98c91ff6ea58fa6885005483c38
seunghye910326 <121917723+seunghye910326@users.noreply.github.com>
2023-01-09 10:20:48 +0900

    김승혜
    marketService 인터페이스 메소드 추가

--- Nongbusim/src/main/java/com/kh/nbs/market/model/service/MarketService.java
+++ Nongbusim/src/main/java/com/kh/nbs/market/model/service/MarketService.java
@@ -1,5 +1,58 @@
 package com.kh.nbs.market.model.service;
 
-public interface MarketService {
+import java.util.ArrayList;
 
+import com.kh.nbs.common.model.vo.Comment;
+import com.kh.nbs.common.model.vo.PageInfo;
+import com.kh.nbs.common.model.vo.Recomment;
+import com.kh.nbs.market.model.vo.Market;
+
+public interface MarketService {
+	
+	
+	//게시글 총 갯수 조회 메소드
+	//게시판 조회용 메소드
+	int selectListCount();
+	ArrayList<Market> marketSelectList(PageInfo pi);
+	
+	//게시물 상세조회 메소드
+	Market marketDetailView(int marketNo);
+
+	//게시물 작성 메소드
+	int increaseCount(int boardNo);
+	int marketInsertForm(Market market);
+	
+	//게시물 수정 메소드
+	int marketUpdate(Market market);
+	
+	//게시물 삭제 메소드
+	int marketDelete(int merketNo);
+	
+	//댓글 리스트 조회 메소드
+	ArrayList<Comment> selectCommentList(int marketNo);
+	
+	//댓글 작성 메소드
+	int insertComment(Comment c);
+	
+	//댓글 삭제 메소드
+	int deleteComment(int cno);
+	
+	//댓글 수정 메소드
+	int updateComment(int cno);
+	
+	//대댓글 리스트 조회 메소드
+	ArrayList<Recomment> selectRecommentList(int cno);
+	
+	//대댓글 작성 메소드
+	int insertRecomment(Recomment r);
+	
+	//대댓글 수정 메소드
+	int updateRecomment(int rcno);
+	
+	//대댓글 삭제 메소드
+	int deleteRecomment(int rcno);
+	
+	//좋아요 메소드
+	
+	
 }
--- Nongbusim/src/main/java/com/kh/nbs/market/model/service/MarketServiceImpl.java
+++ Nongbusim/src/main/java/com/kh/nbs/market/model/service/MarketServiceImpl.java
@@ -1,5 +1,102 @@
 package com.kh.nbs.market.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.nbs.common.model.vo.Comment;
+import com.kh.nbs.common.model.vo.PageInfo;
+import com.kh.nbs.common.model.vo.Recomment;
+import com.kh.nbs.market.model.vo.Market;
+
 public class MarketServiceImpl implements MarketService{
+
+	@Override
+	public int selectListCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Market> marketSelectList(PageInfo pi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Market marketDetailView(int marketNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int increaseCount(int boardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int marketInsertForm(Market market) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int marketUpdate(Market market) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int marketDelete(int merketNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Comment> selectCommentList(int marketNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertComment(Comment c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteComment(int cno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateComment(int cno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Recomment> selectRecommentList(int cno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertRecomment(Recomment r) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateRecomment(int rcno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteRecomment(int rcno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
