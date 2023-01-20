0d0faba2b260160db3db15dab96125930406a34b
SangminLee <121650362+fara2828@users.noreply.github.com>
2023-01-20 12:49:51 +0900

    0120 오전
    상민

--- Nongbusim/src/main/java/com/kh/nbs/account/model/service/AccountServiceImpl.java
+++ Nongbusim/src/main/java/com/kh/nbs/account/model/service/AccountServiceImpl.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.nbs.account.model.dao.AccountDao;
 import com.kh.nbs.account.model.vo.Account;
 import com.kh.nbs.common.model.vo.Attachment;
 import com.kh.nbs.common.model.vo.PageInfo;
